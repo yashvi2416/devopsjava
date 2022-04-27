645f3becdcbde15fa6a6ffab05c02b238c1c9c8d
yashvi2416 <63864415+yashvi2416@users.noreply.github.com>
2022-04-27 12:20:39 +0530

    Add files via upload

---
+++ Differ.java
@@ -0,0 +1,9 @@
+public class Differ{
+public static void main(String args[]){
+int num1=35,num2=56,sub;
+sub=num2-num1;//find subtract of two numbers
+System.out.println("Subtract of two numbers: "+sub);
+
+
+}
+}
